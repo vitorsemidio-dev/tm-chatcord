54f7ef4319e455de35fe003a1c1e5fc81f9a4bf8
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-04-10 20:32:53 -0300

    emit, broadcast, io.emit

--- server.js
+++ server.js
@@ -10,10 +10,14 @@ const io = socketio(server);
 app.use(express.static(path.join(__dirname, 'public')));
 
 io.on('connection', socket => {
-  console.log('New WS Connection...');
-
   socket.emit('message', 'Welcome to chatcord');
-})
+
+  socket.broadcast.emit('message', 'A new user has joined the chat');
+
+  socket.on('disconnect', () => {
+    io.emit('message', 'A user has left the chat');
+  })
+});
 
 const PORT = 3000 || process.env.PORT;
 
